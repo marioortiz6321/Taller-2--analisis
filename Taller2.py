166807a85f5efbe9a9f8cdb12fcfddb225545bb8
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-02-21 10:00:36 -0500

    best now

--- Taller2.py
+++ Taller2.py
@@ -1,20 +1,24 @@
 def BiggestI_Difference(S):
-    n = len(S)
     max_diff = float('-inf')
     pos = None
-    left_sums = [0] * (n+1)  # Almacena la suma acumulada de los elementos a la izquierda
-    for i in range(1, n+1):
-        left_sums[i] = left_sums[i-1] + S[i-1]
-    right_sum = 0
-    for i in range(n-1, 0, -1):
-        right_sum += S[i]
-        diff = left_sums[i] - right_sum
-        if diff > max_diff:
-            max_diff = diff
+    for i in range(1, len(S)+1):
+        left_sum = sumarSub(S, 1, i-1)
+        right_sum = sumarSub(S, i+1, len(S))
+        if max_diff < (left_sum - right_sum):
+            max_diff = left_sum - right_sum
             pos = i
     return (max_diff, pos)
+def sumarSub(S, l, r):
+    if l > r:
+        return 0
+    elif r - l + 1 <= 1000:
+        return sum(S[l-1:r])
+    else:
+        q = (r - l) // 2
+        left_sum = sumarSub(S, l, l+q)
+        right_sum = sumarSub(S, l+q+1, r)
+        return left_sum + right_sum
 
-S = [10, 5, 3, 4, 2, 1,5]
-max_diff, pos = BiggestI_Difference(S)
-print("El mayor valor de la diferencia entre la suma de los elementos a la izquierda y a la derecha es: ", max_diff)
-print("La posición del elemento que maximiza la diferencia es: ", pos)
+S = [1,2,3,4,5]
+max,pos=BiggestI_Difference(S)
+print("La diferencia maxima es: ",max," y se encuentra en la posicion: ",pos)
