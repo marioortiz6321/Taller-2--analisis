b2808090c11af2c51f63274fc8e345f93b8a6710
marioortiz6321 <marioortiz@javeriana.edu.co>
2023-02-21 11:25:38 -0500

    Good now 2

--- Taller2.py
+++ Taller2.py
@@ -2,23 +2,25 @@ def BiggestI_Difference(S):
     max_diff = float('-inf')
     pos = None
     for i in range(1, len(S)+1):
-        left_sum = sumarSub(S, 1, i-1)
-        right_sum = sumarSub(S, i+1, len(S))
+        left_sum = sumarSub(S[:i-1])
+        right_sum = sumarSub(S[i+1:])
         if max_diff < (left_sum - right_sum):
             max_diff = left_sum - right_sum
             pos = i
     return (max_diff, pos)
-def sumarSub(S, l, r):
-    if l > r:
+
+def sumarSub(S):
+    if len(S) == 0:
         return 0
-    elif r - l + 1 <= 1000:
-        return sum(S[l-1:r])
+    if len(S) == 1:
+        return S[0]
     else:
-        q = (r - l) // 2
-        left_sum = sumarSub(S, l, l+q)
-        right_sum = sumarSub(S, l+q+1, r)
+        mid = len(S) // 2
+        left_sum = sumarSub(S[:mid])
+        right_sum = sumarSub(S[mid:])
         return left_sum + right_sum
 
-S = [1,2,3,4,5]
+
+S = [1,2,5,6,7,9]
 max,pos=BiggestI_Difference(S)
 print("La diferencia maxima es: ",max," y se encuentra en la posicion: ",pos)
